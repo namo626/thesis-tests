bbc774aa7585b18cdaf987d3964abecf4ea81fe5
namo626 <namo26june@gmail.com>
2025-03-26 18:22:07 -0500

    Add PE to test wet/dry on floodplain

--- select_PE.py
+++ select_PE.py
@@ -9,13 +9,16 @@ Example:
 #!/usr/bin/env python3
 import utils_beach as ub
 import os
+import sys
 import pandas as pd
 
-if __name__ == "main":
-    y = 8.782
-    x = -81.304
+if __name__ == "__main__":
+    # y = 8.782
+    # x = -81.304
 
-    proj = "120m_nolevee"
+    x = float(sys.argv[2])
+    y = float(sys.argv[3])
+    proj = sys.argv[1]
     folder = os.path.abspath(proj)
     domains = list(filter(lambda x: "PE" in x, os.listdir(folder)))
 
